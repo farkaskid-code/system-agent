2a28eaa918537bf35abebd154a1fc1424330129a
farkaskid <farkaskid.inbox@gmail.com>
2026-09-21 12:32:38 +0530

    Task 2: tool-executor
    Squash merge task-2-tool-executor into dev

--- src/system_agent/__init__.py
+++ src/system_agent/__init__.py
@@ -1,10 +1,39 @@
+import logging
 import sys
+from os import getenv
+from pathlib import Path
 
-from .core.llm_client import LLMClient
+from system_agent.core.llm_client import LLMClient
+from system_agent.core.tool_executor import execute
+
+logging.basicConfig(level=logging.DEBUG, format="%(levelname)s - %(message)s")
+logger = logging.getLogger(__name__)
+
+
+def get_declared_tools(path: Path) -> list[dict]:
+    try:
+        logger.debug(f"loading tools from: {path}")
+        from yaml import safe_load
+
+        return safe_load(path.read_text())["tools"]
+    except (FileNotFoundError, KeyError) as e:
+        raise RuntimeError(f"Failed in loading tools because: {e}")
 
 
 def main() -> None:
-    url = "http://192.168.137.1:11434/v1/chat/completions"
+    OLLAMA_API_BASE = getenv("OLLAMA_API_BASE")
+    url = f"{OLLAMA_API_BASE}/v1/chat/completions"
     model_name = "qwen2.5-coder:14b"
 
-    print(LLMClient(url, model_name).call_model(sys.argv[1]))
+    TOOL_DECLARATIONS = Path("src/system_agent/config/tool_declarations.yaml")
+
+    declared_tools = get_declared_tools(TOOL_DECLARATIONS)
+    client = LLMClient(url=url, model_name=model_name, tools=declared_tools)
+
+    tool = client.get_tool(sys.argv[1])
+    result = execute(tool=tool)
+
+    if result.status:
+        logger.info(result.stdout.raw)
+    else:
+        logger.info(result.stderr.raw)
--- src/system_agent/core/llm_client.py
+++ src/system_agent/core/llm_client.py
@@ -1,39 +1,71 @@
+import logging
 from json import loads
 
 from requests import post
 from requests.exceptions import RequestException
 
-from .parser import map_to_command
-from .tool_executor import CLITool, available_tools
+from system_agent.core.tool_executor import Tool
+
+logger = logging.getLogger(__name__)
 
 
 class LLMClient:
-    def __init__(self, url: str, model_name: str) -> None:
+    def __init__(self, url: str, model_name: str, tools: list[dict]) -> None:
         self.model_name = model_name
         self.temperature = 0.3
         self.url = url
+        self.tools = tools
 
-    def call_model(self, query: str) -> CLITool:
+    def get_tool(self, query: str) -> Tool:
         messages = [{"role": "user", "content": query}]
-        body = {
-            "model": self.model_name,
-            "messages": messages,
-            "temperature": self.temperature,
-            "stream": False,
-            "tools": available_tools(),
-        }
+
+        def _as_spec(tool_declaration: dict) -> dict:
+            params = {}
+            if len(tool_declaration.get("parameters")):
+                properties = {}
+                for param in tool_declaration.get("parameters"):
+                    properties[param["name"]] = {
+                        "type": param["type"],
+                        "description": param["description"],
+                    }
+                params = {"type": "object", "properties": properties}
+            return {
+                "type": "function",
+                "function": {
+                    "name": tool_declaration.get("name"),
+                    "description": tool_declaration.get("description"),
+                    "parameters": params,
+                },
+            }
 
         try:
+            body = {
+                "model": self.model_name,
+                "messages": messages,
+                "temperature": self.temperature,
+                "stream": False,
+                "tools": list(map(_as_spec, self.tools)),
+            }
+            logger.debug(
+                f"calling LLM with query: '{query}' with tools: {[tool['name'] for tool in self.tools]}"
+            )
             response = post(self.url, json=body)
             content = response.json()["choices"][0]["message"]["content"]
+            logger.debug(f"LLM response: {content}")
             tool_call = loads(content)
-            tool = CLITool(tool_call["name"])
-            tool.args = tool_call["arguments"]
-            return tool
-        except (RequestException, KeyError) as e:
-            print(f"LLM request failed because of {e}")
-            print("Using manual mapping rules")
-            return map_to_command(query)
 
-    def __str__(self) -> str:
-        return self.model_name
+            for tool in self.tools:
+                if tool["name"] == tool_call["name"]:
+                    return Tool(
+                        name=tool["name"],
+                        command=tool["command"],
+                        args=list(tool_call["arguments"].values()),
+                    )
+            return Tool(
+                name="echo", command="echo", args=["couldn't detect the command"]
+            )
+        except (RequestException, KeyError) as e:
+            logger.error(f"LLM request failed: {e}")
+            return Tool(
+                name="echo", command="echo", args=["couldn't detect the command"]
+            )
